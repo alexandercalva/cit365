eaa44a80b4e5e4020d2e2e4847c6287a073a4c47
calva-alexander <38995873+alexandercalva@users.noreply.github.com>
2020-01-04 12:43:49 -0500

    Week 1
    The first assignment

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alexander Calva";
+            string location = "Ecuador";
+            DateTime dateTime = DateTime.Today;
+            
+           
+            // Name and location
+            Console.WriteLine("My name is " + name + ", I am from " + location);
+            
+            // Date without the current time
+            Console.WriteLine(" Date: " + dateTime.ToString("dd/MM/yyyy"));
+            
+            // Number of days until Christmas this year
+            DateTime date2 = new DateTime(2020, 12, 25);
+            TimeSpan interval = date2 - dateTime;
+            Console.WriteLine("Days until christmas in this year: " + interval.ToString("dd"));
+
+            //Add the program example from section 2.1 in the C# Programming Yellow Book by Rob Miles.
+            double width, height, woodLength, glassArea; 
+            string widthString, heightString;
+            Console.WriteLine("Enter the width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Enter the height: ");
+            heightString = Console.ReadLine(); 
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " + woodLength + " feet"); Console.WriteLine("The area of the glass is " + glassArea + " square metres");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadKey();
+        }
+
+       
+
+    }
+
+   
+}
